b4c3aca81e2691d4978ab4834b372469b0939acb
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2025-03-28 11:36:22 +0200

    clock function

--- clock/src/App.jsx
+++ clock/src/App.jsx
@@ -1,5 +1,30 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  let milliseconds = 0;
+  let seconds = 0;
+  let minutes = 0;
+
+  const clock = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  return (
+    <main>
+      <button className="clock">Clock</button>
+      <button className="timer">Timer</button>
+      <button className="stopwatch">Stopwatch</button>
+
+      <h1>
+        {`0${minutes}`}:{`0${seconds}`}:{`0${milliseconds}`}
+      </h1>
+    </main>
+  );
+}
 
 export default App;
